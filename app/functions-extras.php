c12fea99ee1fba9d81125c01f26071359c4a5262
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-19 09:13:56 -0700

    initial commit

---
+++ app/functions-extras.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Default extras template
+ *
+ * @package   Creativity
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/creativity
+ */
+
+/**
+ * Changes the theme template path to the `public/views` folder.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return string
+ */
+add_filter( 'backdrop/template/path', function() {
+
+	return 'public/views';
+} );
