e02e5b06015e07954d6dae9d98e6c4e1d883a321
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-19 09:06:32 -0700

    initial commit

---
+++ app/functions-setup.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Default theme setup.
+ *
+ * This is where all the add_theme_support(); will happen.
+ *
+ * @package   Creativity
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/creativity
+ */
+
+namespace Creativity;
+
+/**
+ * Set up theme support.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
+
+add_action( 'after_setup_theme', function() {
+
+	// Set the theme content width.
+	$GLOBALS['content_width'] = 640;
+
+	// Load theme translations.
+	load_theme_textdomain( 'creativity', get_parent_theme_file_path( 'public/lang' ) );
+
+	// Automatically add the `<title>` tag.
+	add_theme_support( 'title-tag' );
+
+	// Automatically add feed links to `<head>`.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Adds featured image support.
+	add_theme_support( 'post-thumbnails' );
+
+	// Outputs HTML5 markup for core features.
+	add_theme_support( 'html5', [
+		'caption',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'search-form'
+	] );
+}, 5 );
