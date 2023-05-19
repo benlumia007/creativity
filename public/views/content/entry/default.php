6fa51d8d5298407a5f4d55075216e64a4e027d66
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-19 11:08:44 -0700

    create a loop grid system for the main template for blog

--- public/views/content/entry/default.php
+++ public/views/content/entry/default.php
@@ -9,23 +9,14 @@
  * @link      https://github.com/benlumia007/inheritance
  */
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php Backdrop\Theme\Entry\display_title(); ?>
-	</header>
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'succotash' ),
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'succotash' ) . ' </span>%',
-				'separator'   => '<span class="screen-reader-text">,</span> ',
-			)
-		);
-		?>
-	</div>
-</article>
+<li class="grid-item">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php Backdrop\Theme\Entry\display_title(); ?>
+		</header>
+		<div class="entry-content">
+			<?php the_excerpt(); ?>
+		</div>
+	</article>
+</li>
+`
