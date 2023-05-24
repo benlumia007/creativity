d1274b11df7776f27376f46312d3a9bb52c6a116
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-23 18:32:02 -0700

    updated

--- functions.php
+++ functions.php
@@ -21,3 +21,20 @@
 if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
 	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
 }
+
+# ------------------------------------------------------------------------------
+# Autoload functions files.
+# ------------------------------------------------------------------------------
+#
+# Load any functions-files from the `/app` folder that are needed. Add additional
+# files to the array without the `.php` extension.
+
+array_map( function( $file ) {
+	require_once( get_parent_theme_file_path( "app/{$file}.php" ) ); // phpcs:ignore WPThemeReview.CoreFunctionality.FileInclude.FileIncludeFound
+}, [
+	'Site/functions-site',
+	'framework',
+	'functions-extras',
+	'functions-scripts',
+	'functions-setup',
+] );
